45f79a7cfb3dcd192b39604b9c9b85ac92eab2d8
Sharren <sharrenepf@gmail.com>
2025-03-16 12:55:23 +0700

    Percobaan 1

---
+++ Faktorial24.java
@@ -0,0 +1,18 @@
+class Faktorial24 {
+    
+    int faktorialBF(int n) {
+        int fakto = 1;
+        for (int i = 1; i <= n; i++) {
+            fakto = fakto * i;
+        }
+        return fakto;
+    }
+
+    int faktorialDC(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * faktorialDC(n - 1);
+        }
+    }
+}
